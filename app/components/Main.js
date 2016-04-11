ef322b6756a3055845563d433b61bf83f229a830
Manju Muthaiya <manju.muthaiya@gmail.com>
2016-04-11 07:34:12 -0700

    Adds Search feature

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchGithub from './SearchGithub';
 
 class Main extends React.Component {
 
@@ -8,7 +9,7 @@ class Main extends React.Component {
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
+            <SearchGithub />
           </div>
         </nav>
         <div className="container">
---
+++ app/components/SearchGithub.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from 'react-router';
+
+class SearchGithub extends React.Component {
+  getRef(ref) {
+    this.usernameRef = ref;
+  }
+
+  handleSubmit() {
+    var username = this.usernameRef.value;
+    this.usernameRef.value = '';
+    this.context.router.push("profile/" + username);
+  }
+
+  render() {
+    return (
+      <div className="col-sm-12">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="form-group col-sm-7">
+            <input type="text" className="form-control" ref={this.getRef.bind(this)}/>
+          </div>
+          <div className="form-group col-sm-5">
+            <button type="submit" className="btn btn-block btn-primary">Search Github</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+}
+SearchGithub.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+export default SearchGithub;
+
